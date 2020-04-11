ce4c02c7278cd0fb3f283048ae08a6ff4291d4c3
Thabang Mabula <1364103@students.wits.ac.za>
2020-04-11 10:10:35 +0200

    Added docstrings to home

--- js/home.js
+++ js/home.js
@@ -4,18 +4,8 @@ var modelDisplayArea = $('.model-area')
 var modelFileName = 'character.glb'
 var modelDisplayer = new ModelDisplayer(modelFileName, modelDisplayArea)
 
-// displayModelOnWebpage(modelFileName, modelDisplayArea)
-//   .then((model, mixer) => {
-//     playAnimation(model, mixer, 0)
-//   }).catch((err) => {
-//     console.error(err)
-//   })
 $(document).ready(async function () {
   await modelDisplayer.displayModelOnWebpage(modelFileName, modelDisplayArea)
-  // modelDisplayer.displayDeactivateCircle()
-  // modelDisplayer.displayMotionRegions()
-  // modelDisplayer.playAnimation(0)
-  // console.log(modelDisplayer.displayMotionRegions())
 
   $('body').mousemove(function (event) {
     let mousePosition = { x: event.pageX, y: event.pageY }
@@ -23,6 +13,8 @@ $(document).ready(async function () {
     let activationRegion = modelDisplayer.getActivationRegion()
     let length = 4
 
+    // Check first if the mouse is outside of the activationRegion
+    // If so, play an animaiton to revert the character to its original position
     if (!isWithinTriggerRegion(mousePosition, activationRegion)) {
       modelDisplayer.revertToOriginalPosition()
     } else {
@@ -34,18 +26,15 @@ $(document).ready(async function () {
     }
   })
 
-  $('#revert').click(() => {
-    modelDisplayer.revertToOriginalPosition()
-  })
-
-  function animate () {
-    requestAnimationFrame(animate)
-    modelDisplayer.animate()
-  }
-
   animate()
 })
 
+/**
+ * Checks whether the mouse position is within the model's animation trigger regions
+ * @param {Object} mousePosition Object containing the x and y coordinates of the mouse
+ * @param {Object} triggerRegionKeyPoints Object containign the x and y coordinates of the trigger region's centre and top-most point
+ * @returns {boolean} A boolean indicating whether the mouse is within the circular trigger region
+ */
 function isWithinTriggerRegion (mousePosition, triggerRegionKeyPoints) {
   let radius = euclidianDistance(triggerRegionKeyPoints.center, triggerRegionKeyPoints.top)
   let distanceFromMouseToCentre = euclidianDistance(triggerRegionKeyPoints.center, mousePosition)
@@ -53,6 +42,19 @@ function isWithinTriggerRegion (mousePosition, triggerRegionKeyPoints) {
   return distanceFromMouseToCentre <= radius
 }
 
+/**
+ * Calcualtes the euclideanDistance between two points
+ * @param {Object} p1 A two-dimentional point with an x and y coordinate
+ * @param {Object} p2 A two-dimentional point with an x and y coordinate
+ */
 function euclidianDistance (p1, p2) {
   return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2))
+}
+
+/**
+ * Start the main animation loop
+ */
+function animate () {
+  requestAnimationFrame(animate)
+  modelDisplayer.animate()
 }
